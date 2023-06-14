7c625499707b16f47bece92ac14d6561ad8e39f9
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-06-14 21:52:27 +0300

    in name of allah

---
+++ src/main/java/com/global/mentorship/MentwayApplication.java
@@ -0,0 +1,13 @@
+package com.global.mentorship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/mentorship/MentwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.mentorship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
