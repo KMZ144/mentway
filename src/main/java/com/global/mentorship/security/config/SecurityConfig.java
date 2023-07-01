e28c20579d9d1b748816ac398b0e1b69b37b11d1
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-07-01 16:51:54 +0300

    finish

--- src/main/java/com/global/mentorship/security/config/SecurityConfig.java
+++ src/main/java/com/global/mentorship/security/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig  {
 			return new WebMvcConfigurer() {
 				@Override
 				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("**/**").allowedOrigins("http://localhost:4200");
+	                registry.addMapping("/**").allowedOrigins("*");
 				}
 			};
 		}	
