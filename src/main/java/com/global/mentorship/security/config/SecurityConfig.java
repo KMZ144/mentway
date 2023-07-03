07c91f29375bd4693943eae1c3bda17140bd1488
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-07-03 12:51:54 +0300

    editing auth

--- src/main/java/com/global/mentorship/security/config/SecurityConfig.java
+++ src/main/java/com/global/mentorship/security/config/SecurityConfig.java
@@ -49,6 +49,8 @@ public class SecurityConfig  {
 		.cors().and()
 		.authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/auth/**").permitAll())
 		.authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/auth/register/mentor").permitAll())
+		.authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/auth/register/mentee").permitAll())
+		.authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/categories").permitAll())
 		.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET,"api/v1/mentors/**").permitAll())
 		.authorizeHttpRequests(auth -> auth.requestMatchers("swagger-ui/**","v3/api-docs/**").permitAll())
 		.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
