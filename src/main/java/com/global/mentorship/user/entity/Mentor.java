f03d50f3fea34015ee29e17996dbebe5d00ff545
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-07-03 19:23:29 +0300

    mentor is valid

--- src/main/java/com/global/mentorship/user/entity/Mentor.java
+++ src/main/java/com/global/mentorship/user/entity/Mentor.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
@@ -20,6 +21,7 @@ public class Mentor extends User{
 	@Lob
 	private String coverLetter;
 	
+	@Column(columnDefinition = "boolean default true")
 	private boolean isValid;
 	
 	private String cvUrl;
